6733954d35fd5364068520f1c30921e1c116cc4d
valdozzz <edvaldo99@icloud.com>
2022-09-24 15:17:40 +0200

    hot fix

--- app/controllers/user/likes.js
+++ app/controllers/user/likes.js
@@ -6,25 +6,69 @@ const Likes = mongoose.model('Like');
 
 const tagLabel = "userLikesReadPublicController";
 
+const setContextIfUserLogged = async (req) => {
+    try {
+
+        let token = req.headers['x-auth-token'];
+
+        if (typeof token !== 'string' || token === "")
+            return;
+
+        let decodedUser;
+
+        try {
+            decodedUser = jwt.verify(token, process.env.JWT_KEY);
+
+        } catch (error) {
+            return;
+        }
+
+        if (!decodedUser) {
+            return;
+        }
+
+        const query = { _id: decodedUser._id };
+
+        const user = await Users.findOne(query);
+
+        if (!user)
+            return;
+
+        req.locals.user = user;
+        httpContext.set("context", user);
+        return await httpContext.ns.runPromise(async () => {
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+};
+
 new utilities.express.Service(tagLabel)
     .isGet()
     .respondsAt('/users/:username/likes')
     .isPublic()
     .controller(async (req, res) => {
 
+        await setContextIfUserLogged(req);
+
         const user = await Users.findOne({ username: req.params.username });
         if (!user) {
             return res.notFound("User not found");
         }
 
-        const likes = await Likes.aggregate([
+        const { page = 0, limit = 20 } = req.query;
+
+
+        const aggreagate = [
             {
                 '$group': {
                     '_id': '$post',
                     'likes': {
                         '$sum': 1
                     },
-                    'isLiked': {
+                    'hasLiked': {
                         '$sum': {
                             '$cond': [
                                 {
@@ -38,7 +82,7 @@ new utilities.express.Service(tagLabel)
                 }
             }, {
                 '$match': {
-                    'isLiked': 1
+                    'hasLiked': 1
                 }
             }, {
                 '$lookup': {
@@ -95,13 +139,42 @@ new utilities.express.Service(tagLabel)
                     "hunter.hashPassword": 0,
                     "hunter.email": 0,
                     "artist.__v": 0,
-                    "hunter.__v": 0
+                    "hunter.__v": 0,
+                    hasLiked: 0
 
                 }
+            },
+            {
+                "$sort": {
+                    "likes": -1,
+                    createdAt: -1
+                }
+            }, {
+                "$skip": page * limit
+            },
+            {
+                "$limit": limit
             }
 
-        ])
+        ]
 
+        console.log(req.locals.user);
+        if (req?.locals?.user) {
+            console.log('here');
+            aggreagate[0].$group.isLiked = {
+                '$sum': {
+                    '$cond': [
+                        {
+                            '$eq': [
+                                '$user', req.locals.user._id
+                            ]
+                        }, 1, 0
+                    ]
+                }
+            }
+        }
+
+        const likes = await Likes.aggregate(aggreagate)
 
         res.resolve(likes);
 
