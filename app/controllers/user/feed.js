2a91b12cef8c1389389b53cc8136bf5b1d227b74
mattiapomelli <mattiapomelli@gmail.com>
2022-10-09 10:20:05 +0200

    Add pagination on songs feed controller

--- app/controllers/user/feed.js
+++ app/controllers/user/feed.js
@@ -8,6 +8,7 @@ const jwt = require("jsonwebtoken");
 
 const tagLabel = "feedController";
 
+const PER_PAGE = 20;
 
 const setContextIfUserLogged = async (req) => {
     try {
@@ -58,184 +59,190 @@ new utilities.express.Service(tagLabel)
 
         const { page = 0, limit = 20, date = new Date() } = req.query;
 
-        const feed = await Posts.aggregate(
-            [
-                {
-                    '$match': {
-                        'status': 'ONLINE'
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'artists',
-                        'localField': 'artist',
-                        'foreignField': '_id',
-                        'as': 'artist'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$artist'
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'users',
-                        'localField': 'hunter',
-                        'foreignField': '_id',
-                        'as': 'hunter'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$hunter'
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'likes',
-                        'let': {
-                            'id': '$_id'
-                        },
-                        'pipeline': [
-                            {
-                                '$match': {
-                                    '$expr': {
-                                        '$eq': [
-                                            '$post', '$$id'
-                                        ]
-                                    }
-                                }
-                            }, {
-                                '$group': {
-                                    '_id': '$post',
-                                    'likes': {
-                                        '$sum': 1
-                                    },
-                                    'isLiked': {
-                                        '$sum': {
-                                            '$cond': [
-                                                {
-                                                    '$eq': [
-                                                        '$user', req?.locals?.user?._id
-                                                    ]
-                                                }, 1, 0
-                                            ]
-                                        }
-                                    }
-                                }
-                            }, {
-                                '$project': {
-                                    '_id': 0
+        const query =             [
+            {
+                '$match': {
+                    'status': 'ONLINE'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'artists',
+                    'localField': 'artist',
+                    'foreignField': '_id',
+                    'as': 'artist'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$artist'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'users',
+                    'localField': 'hunter',
+                    'foreignField': '_id',
+                    'as': 'hunter'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$hunter'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'likes',
+                    'let': {
+                        'id': '$_id'
+                    },
+                    'pipeline': [
+                        {
+                            '$match': {
+                                '$expr': {
+                                    '$eq': [
+                                        '$post', '$$id'
+                                    ]
                                 }
                             }
-                        ],
-                        'as': 'likes'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$likes',
-                        'preserveNullAndEmptyArrays': true
-                    }
-                }, {
-                    '$replaceRoot': {
-                        'newRoot': {
-                            '$mergeObjects': [
-                                '$$ROOT', '$likes'
-                            ]
-                        }
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'likes',
-                        'let': {
-                            'id': '$_id'
-                        },
-                        'pipeline': [
-                            {
-                                '$match': {
-                                    '$expr': {
-                                        '$and': [
+                        }, {
+                            '$group': {
+                                '_id': '$post',
+                                'likes': {
+                                    '$sum': 1
+                                },
+                                'isLiked': {
+                                    '$sum': {
+                                        '$cond': [
                                             {
                                                 '$eq': [
-                                                    '$post', '$$id'
+                                                    '$user', req?.locals?.user?._id
                                                 ]
-                                            }, {
-                                                '$gte': [
-                                                    '$createdAt', new Date(moment(date).startOf("day"))
-                                                ]
-                                            }, {
-                                                '$lte': [
-                                                    '$createdAt', new Date(moment(date).endOf("day"))
-                                                ]
-                                            }
+                                            }, 1, 0
                                         ]
                                     }
                                 }
-                            }, {
-                                '$group': {
-                                    '_id': '$post',
-                                    'partialLikes': {
-                                        '$sum': 1
-                                    }
-                                }
-                            }, {
-                                '$project': {
-                                    '_id': 0
+                            }
+                        }, {
+                            '$project': {
+                                '_id': 0
+                            }
+                        }
+                    ],
+                    'as': 'likes'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$likes',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }, {
+                '$replaceRoot': {
+                    'newRoot': {
+                        '$mergeObjects': [
+                            '$$ROOT', '$likes'
+                        ]
+                    }
+                }
+            }, {
+                '$lookup': {
+                    'from': 'likes',
+                    'let': {
+                        'id': '$_id'
+                    },
+                    'pipeline': [
+                        {
+                            '$match': {
+                                '$expr': {
+                                    '$and': [
+                                        {
+                                            '$eq': [
+                                                '$post', '$$id'
+                                            ]
+                                        }, {
+                                            '$gte': [
+                                                '$createdAt', new Date(moment(date).startOf("day"))
+                                            ]
+                                        }, {
+                                            '$lte': [
+                                                '$createdAt', new Date(moment(date).endOf("day"))
+                                            ]
+                                        }
+                                    ]
                                 }
                             }
-                        ],
-                        'as': 'partialLikes'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$partialLikes',
-                        'preserveNullAndEmptyArrays': true
-                    }
-                }, {
-                    '$replaceRoot': {
-                        'newRoot': {
-                            '$mergeObjects': [
-                                '$$ROOT', '$partialLikes'
-                            ]
+                        }, {
+                            '$group': {
+                                '_id': '$post',
+                                'partialLikes': {
+                                    '$sum': 1
+                                }
+                            }
+                        }, {
+                            '$project': {
+                                '_id': 0
+                            }
                         }
-                    }
-                }, {
-                    '$set': {
-                        'likes': {
-                            '$ifNull': [
-                                '$likes', 0
-                            ]
-                        },
-                        'isLiked': {
-                            '$ifNull': [
-                                '$isLiked', 0
-                            ]
-                        },
-                        'partialLikes': {
-                            '$ifNull': [
-                                '$partialLikes', 0
-                            ]
-                        }
-                    }
-                },
-                {
-                    $project: {
-                        "hunter.isAdmin": 0,
-                        "hunter.emailConfirmation": 0,
-                        "hunter.hashPassword": 0,
-                        "hunter.email": 0,
-
-                    }
-                },
-                {
-                    "$sort": {
-                        "partialLikes": -1,
-                        createdAt: -1
-                    }
-                }, {
-                    "$skip": page * limit
-                },
-                {
-                    "$limit": limit
+                    ],
+                    'as': 'partialLikes'
                 }
-            ]
-        )
+            }, {
+                '$unwind': {
+                    'path': '$partialLikes',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }, {
+                '$replaceRoot': {
+                    'newRoot': {
+                        '$mergeObjects': [
+                            '$$ROOT', '$partialLikes'
+                        ]
+                    }
+                }
+            }, {
+                '$set': {
+                    'likes': {
+                        '$ifNull': [
+                            '$likes', 0
+                        ]
+                    },
+                    'isLiked': {
+                        '$ifNull': [
+                            '$isLiked', 0
+                        ]
+                    },
+                    'partialLikes': {
+                        '$ifNull': [
+                            '$partialLikes', 0
+                        ]
+                    }
+                }
+            },
+            {
+                $project: {
+                    "hunter.isAdmin": 0,
+                    "hunter.emailConfirmation": 0,
+                    "hunter.hashPassword": 0,
+                    "hunter.email": 0,
+
+                }
+            },
+            {
+                "$sort": {
+                    "partialLikes": -1,
+                    createdAt: -1
+                }
+            }, {
+                "$skip": page * limit
+            },
+            {
+                "$limit": limit
+            }
+        ]
+
+        const feed = await Posts.aggregate(query)
+
+        res.setPagination({
+            total: await Posts.countDocuments(query),
+            perPage: PER_PAGE,
+            page
+        });
 
         return res.resolve(feed);
 
