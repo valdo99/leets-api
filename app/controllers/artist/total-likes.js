767114342025fa772a55c01dcec3f47689522ad2
valdozzz <edvaldo99@icloud.com>
2022-10-16 17:17:32 +0200

    hot fix

--- app/controllers/artist/total-likes.js
+++ app/controllers/artist/total-likes.js
@@ -1,6 +1,6 @@
-const moment = require('moment');
 const mongoose = require('mongoose');
 const Posts = mongoose.model("Post");
+const Likes = mongoose.model("Like");
 
 const tagLabel = "totalLikesController";
 
@@ -11,100 +11,17 @@ new utilities.express.Service(tagLabel)
     .respondsAt('/artists/:artist/total-likes')
     .controller(async (req, res) => {
 
+        const postsPerArtist = await Posts.find({
+            status: "ONLINE",
+            artist: req.params.artist
+        })
 
-        const feed = await Posts.aggregate(
-            [
-                {
-                    '$match': {
-                        'status': 'ONLINE',
-                        'artist': mongoose.Types.ObjectId(req.params.artist)
-                    }
-                },
-                {
-                    '$lookup': {
-                        'from': 'artists',
-                        'localField': 'artist',
-                        'foreignField': '_id',
-                        'as': 'artist'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$artist'
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'users',
-                        'localField': 'hunter',
-                        'foreignField': '_id',
-                        'as': 'hunter'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$hunter'
-                    }
-                }, {
-                    '$lookup': {
-                        'from': 'likes',
-                        'let': {
-                            'id': '$_id'
-                        },
-                        'pipeline': [
-                            {
-                                '$match': {
-                                    '$expr': {
-                                        '$eq': [
-                                            '$post', '$$id'
-                                        ]
-                                    }
-                                }
-                            }, {
-                                '$group': {
-                                    '_id': '$post',
-                                    'likes': {
-                                        '$sum': 1
-                                    },
-                                }
-                            }, {
-                                '$project': {
-                                    '_id': 0
-                                }
-                            }
-                        ],
-                        'as': 'likes'
-                    }
-                }, {
-                    '$unwind': {
-                        'path': '$likes',
-                        'preserveNullAndEmptyArrays': true
-                    }
-                }, {
-                    '$replaceRoot': {
-                        'newRoot': {
-                            '$mergeObjects': [
-                                '$$ROOT', '$likes'
-                            ]
-                        }
-                    }
-                }, {
-                    '$set': {
-                        'likes': {
-                            '$ifNull': [
-                                '$likes', 0
-                            ]
-                        }
-                    }
-                },
-                {
-                    $project: {
-                        likes: 1,
-                        _id: 0
-                    }
-                },
+        const likes = await Likes.countDocuments({
+            post: { $in: postsPerArtist }
+        })
 
-            ]
-        )
 
-        return res.resolve(feed[0]);
+        return res.resolve({ likes });
 
 
     });
