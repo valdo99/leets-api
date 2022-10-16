99e2113635982e526ccb784ee508576d73466c93
valdozzz <edvaldo99@icloud.com>
2022-10-16 17:40:31 +0200

    added unted artists

---
+++ app/controllers/user/artists-hunted.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Artist = mongoose.model("Artist");
+const User = mongoose.model("User");
+
+const tagLabel = "artistsHuntedByUserPublicController";
+
+
+new utilities.express.Service(tagLabel)
+    .isGet()
+    .isPublic()
+    .respondsAt('/users/:username/hunted-artists')
+    .controller(async (req, res) => {
+
+        const user = await User.findOne({
+            username: req.params.username
+        })
+
+        if (!user)
+            return res.notFound()
+
+
+
+        const artists = await Artist.find({
+            hunter: user._id
+        })
+
+        return res.resolve(artists);
+
+
+    });
