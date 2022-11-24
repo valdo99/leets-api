afa4b501b38ca45e8a09d39d9adaa40d5d45c2d0
valdozzz <edvaldo99@icloud.com>
2022-11-25 00:06:09 +0100

    hot fix

---
+++ app/controllers/songs/is-liked.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Likes = mongoose.model("Like");
+const tagLabel = "isSongLikesReadController";
+
+const authPublicRoute = utilities.dependencyLocator.get("authPublicRoute");
+
+
+
+new utilities.express.Service(tagLabel)
+    .isGet()
+    .isPublic()
+    .respondsAt("/posts/post/is-liked/:id")
+    .controller(async (req, res) => {
+        await authPublicRoute.setContext(req);
+
+        const post = await Likes.findOne({ post: req.params.id, user: req.locals.user._id });
+
+
+        if (!post) {
+            return res.resolve(false);
+        }
+
+        return res.resolve(true);
+
+    });
