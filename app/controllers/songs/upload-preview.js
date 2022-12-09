34af3948b940562d1a41d2fc3565640a47d87544
valdozzz <edvaldo99@icloud.com>
2022-12-09 12:40:53 +0100

    debug

--- app/controllers/songs/upload-preview.js
+++ app/controllers/songs/upload-preview.js
@@ -48,7 +48,7 @@ const getToken = async () => {
 		type: res.data.token_type,
 		validTill: moment().add(res.data.expires_in, "seconds").toISOString(),
 	});
-
+	console.log('YES TOKEN');
 	await newAccessToken.save();
 	return res.data.access_token;
 };
@@ -62,7 +62,7 @@ const getTrackData = async ({ token, id }) => {
 			Authorization: `Bearer ${token}`,
 		},
 	});
-
+	console.log('YES TRACK DATA');
 	return res.data;
 };
 
@@ -75,6 +75,7 @@ const getArtistData = async ({ token, id }) => {
 			Authorization: `Bearer ${token}`,
 		},
 	});
+	console.log('YES ARTIST DATA');
 
 	return res.data;
 };
@@ -91,85 +92,23 @@ new utilities.express.Service(tagLabel)
 	.isPost()
 	.respondsAt("/posts/upload/preview")
 	.controller(async (req, res) => {
-		try {
-			const token = await getToken();
-			const { id } = req.body;
+		const token = await getToken();
+		const { id } = req.body;
 
-			const existingPost = await Post.findOne({ spotify_id: id }).populate({
-				path: "artist",
-				model: "Artist",
-			});
+		const existingPost = await Post.findOne({ spotify_id: id }).populate({
+			path: "artist",
+			model: "Artist",
+		});
 
-			if (existingPost && ["UPLOADED", "ONLINE"].includes(existingPost.status)) {
-				return res.forbidden(i18n.__("SONG_ALREADY_UPLOADED"));
-			}
+		if (existingPost && ["UPLOADED", "ONLINE"].includes(existingPost.status)) {
+			return res.forbidden(i18n.__("SONG_ALREADY_UPLOADED"));
+		}
 
-			if (existingPost) {
-				// Check max number of weekly songs for the artists hasn't been reached
-				const artistPosts = await getSongsByArtistInLastWeek(
-					existingPost.artist._id,
-				);
-				if (artistPosts.length >= MAX_SONGS_PER_ARTIST_PER_WEEK) {
-					return res.forbidden(
-						i18n.__("MAX_SONGS_PER_ARTIST_REACHED", {
-							max: MAX_SONGS_PER_ARTIST_PER_WEEK,
-						}),
-					);
-				}
-				return res.resolve(existingPost);
-			}
-
-			const { name: title, preview_url, artists, album } = await getTrackData({
-				id,
-				token,
-			});
-
-			const { name: artistName, followers: artistFollowers } =
-				await getArtistData({ id: artists[0].id, token });
-
-			const { getArtist, getTrack } = utilities.dependencyLocator.get("spotify");
-
-			const { playcount, artistsWithRoles, albumOfTrack } = (await getTrack(id))
-				.trackUnion;
-
-			const artistId = artistsWithRoles.items.filter(
-				(el) => el.role === "MAIN",
-			)[0].artist.id;
-			const artistData = await getArtist(artistId);
-
-			const monthlyListeners = artistData.stats.monthlyListeners;
-
-			if (monthlyListeners > 35000) {
-				return res.forbidden(i18n.__("MAX_LISTENERS_PER_ARTIST", { max: 35000 }));
-			}
-
-			let postImage = album.images[0].url;
-
-			const high_quality_image = album.images.filter((el) => el.height === 640);
-			if (high_quality_image.length) { postImage = high_quality_image[0].url; }
-
-			let artist;
-
-			artist = await Artist.findOne({ spotify_id: artistId });
-
-			if (!artist) {
-				artist = new Artist({
-					name: artistName,
-					image: artistData.visuals.avatarImage?.sources[0].url,
-					followers: artistFollowers.total,
-					spotify_id: artistId,
-					// hunter: req.locals.user._id,
-					// headerImage: artistData.visuals.headerImage?.sources[0].url,
-					// biography: artistData.profile.biography.text,
-					// topCities: artistData.stats.topCities.items,
-					// monthly_listeners: monthlyListeners
-				});
-				await artist.save();
-			}
-
+		if (existingPost) {
 			// Check max number of weekly songs for the artists hasn't been reached
-			const artistPosts = await getSongsByArtistInLastWeek(artist._id);
-
+			const artistPosts = await getSongsByArtistInLastWeek(
+				existingPost.artist._id,
+			);
 			if (artistPosts.length >= MAX_SONGS_PER_ARTIST_PER_WEEK) {
 				return res.forbidden(
 					i18n.__("MAX_SONGS_PER_ARTIST_REACHED", {
@@ -177,28 +116,84 @@ new utilities.express.Service(tagLabel)
 					}),
 				);
 			}
-
-			const newPost = new Post({
-				title,
-				image: postImage,
-				preview_url,
-				spotify_id: id,
-				artist: artist._id.toString(),
-				playcount,
-				uploadedAt: albumOfTrack.date.isoString,
-			});
-
-			await (
-				await newPost.save()
-			).populate({
-				path: "artist",
-				model: "Artist",
-			});
-
-			res.resolve(newPost);
-		} catch (error) {
-			console.log(error);
-			return res.status(500).send(error)
+			return res.resolve(existingPost);
 		}
 
+		const { name: title, preview_url, artists, album } = await getTrackData({
+			id,
+			token,
+		});
+
+		const { name: artistName, followers: artistFollowers } =
+			await getArtistData({ id: artists[0].id, token });
+
+		const { getArtist, getTrack } = utilities.dependencyLocator.get("spotify");
+
+		const { playcount, artistsWithRoles, albumOfTrack } = (await getTrack(id))
+			.trackUnion;
+
+		const artistId = artistsWithRoles.items.filter(
+			(el) => el.role === "MAIN",
+		)[0].artist.id;
+		const artistData = await getArtist(artistId);
+
+		const monthlyListeners = artistData.stats.monthlyListeners;
+
+		if (monthlyListeners > 35000) {
+			return res.forbidden(i18n.__("MAX_LISTENERS_PER_ARTIST", { max: 35000 }));
+		}
+
+		let postImage = album.images[0].url;
+
+		const high_quality_image = album.images.filter((el) => el.height === 640);
+		if (high_quality_image.length) { postImage = high_quality_image[0].url; }
+
+		let artist;
+
+		artist = await Artist.findOne({ spotify_id: artistId });
+
+		if (!artist) {
+			artist = new Artist({
+				name: artistName,
+				image: artistData.visuals.avatarImage?.sources[0].url,
+				followers: artistFollowers.total,
+				spotify_id: artistId,
+				// hunter: req.locals.user._id,
+				// headerImage: artistData.visuals.headerImage?.sources[0].url,
+				// biography: artistData.profile.biography.text,
+				// topCities: artistData.stats.topCities.items,
+				// monthly_listeners: monthlyListeners
+			});
+			await artist.save();
+		}
+
+		// Check max number of weekly songs for the artists hasn't been reached
+		const artistPosts = await getSongsByArtistInLastWeek(artist._id);
+
+		if (artistPosts.length >= MAX_SONGS_PER_ARTIST_PER_WEEK) {
+			return res.forbidden(
+				i18n.__("MAX_SONGS_PER_ARTIST_REACHED", {
+					max: MAX_SONGS_PER_ARTIST_PER_WEEK,
+				}),
+			);
+		}
+
+		const newPost = new Post({
+			title,
+			image: postImage,
+			preview_url,
+			spotify_id: id,
+			artist: artist._id.toString(),
+			playcount,
+			uploadedAt: albumOfTrack.date.isoString,
+		});
+
+		await (
+			await newPost.save()
+		).populate({
+			path: "artist",
+			model: "Artist",
+		});
+
+		res.resolve(newPost);
 	});
